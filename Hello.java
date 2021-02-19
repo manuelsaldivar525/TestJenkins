3358ef2270fe477ee6ec16e1e63dbda8c6732c85
manuel saldivar <manuel.saldivar@globant.com>
2021-02-18 22:04:21 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,14 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		for(int i =0; i<10; i++)
+		for(int i =1; i<11; i++)
 		{
-			System.out.println("Hello world Manuel :" + i);
+			System.out.println("Hello world Manuel SR:" + i);
 		}
+		
+		
+		
+	
 
 	}
 
